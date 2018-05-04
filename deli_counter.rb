e797a2428372678d8f1ce7bb332cc0ca3ac4d49a
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 03:53:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,13 @@ index=1
 line_with_num=[]
   if katz_deli.length==0
   puts "The line is currently empty."
-  else
-    katz_deli.each do |x|
+else
+    katz_deli.each { |x|
     line_with_num.push("#{index}. #{x}")
     index+=1
-end
-end
+}
 return "The line is currently: #{line_with_num}"
+end
 end
 
 def take_a_number
