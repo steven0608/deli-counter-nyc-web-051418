65fe144a33d9a62c42720c91205a6b4a5f8da15b
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 04:18:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,13 @@ end
 end
 
 def take_a_number(katz_deli,name)
+if katz_deli.length==0
+  katz_deli<<name
+  puts "Welcome, #{name}. You are number 1 in line."
+else
   katz_deli << name
-
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 end
 
 def now_serving
