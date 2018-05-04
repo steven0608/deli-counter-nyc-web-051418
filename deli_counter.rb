8c10ff4253d2d59a04f3195f6a4e941df8fb42ae
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 03:39:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,17 @@
 # Write your code here.
 def line(katz_deli)
+  array1="The line is currently:"
+  index=1
+
   if katz_deli.length==0
   puts "The line is currently empty."
   else
-
-
+    katz_deli.each do |x|
+    array2="#{index}. #{x}"
+    array1.concate(array2)
+    index+=1
+end
+array1
 end
 
 def take_a_number
